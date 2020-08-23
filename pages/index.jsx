c1ac46a4ab2bc51866b1932e391aa4a330944304
Kenji Imamula <kenji.imamula@gmail.com>
2020-08-23 23:26:53 +0900

    Initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+export default {
+  render(h) {
+    return (
+      <v-container fill-height>
+        <v-layout justify-center align-center>
+          <v-flex shrink>
+            <v-btn
+              icon
+              color="test"
+              large
+              onClick={() =>
+                (this.$vuetify.theme.dark = !this.$vuetify.theme.dark)
+              }
+            >
+              <v-icon>mdi-theme-light-dark</v-icon>
+            </v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    );
+  }
+};
